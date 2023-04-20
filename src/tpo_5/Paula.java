3bfea15a22a8c9eb913953db519ee53502347022
PaulaPriotti <paaupriotti@gmail.com>
2023-04-20 20:37:02 -0300

    Create Paula.java

---
+++ src/tpo_5/Paula.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpo_5;
+
+import com.sun.xml.internal.ws.api.message.saaj.SAAJFactory;
+
+/**
+ *
+ * @author Paula Priotti
+ */
+public class Paula {
+    int suma=0;
+
+    public Paula() {
+    }
+
+    public int getSuma() {
+        return suma;
+    }
+    
+    public void mostrarSuma(){
+        System.out.println("La suma es: "+suma);
+    }
+            
+}
